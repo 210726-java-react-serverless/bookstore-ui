a0b35816a0c900afaa5bbb6c5b26b512d972e0ef
wsingleton <wezley.singleton@gmail.com>
2021-08-19 18:01:03 -0400

    login functionality complete

--- components/navbar/navbar.component.js
+++ components/navbar/navbar.component.js
@@ -1,46 +1,40 @@
+import router from "../../app.js";
+
 const NAVBAR_ELEMENT = document.getElementById('navbar');
 
 function NavbarComponent() {
 
-    let template = `
-    <nav id="navbar-component" class="navbar navbar-expand navbar-dark bg-dark" aria-label="navbar-component">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">Bookstore</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar-collapsible" aria-controls="navbar-collapsible" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-    
-            <div class="collapse navbar-collapse" id="navbar-collapsible">
-                <ul class="navbar-nav me-auto">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a id="logout" class="nav-link" href="#">Logout</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="dropdown03" data-bs-toggle="dropdown" aria-expanded="false">Dropdown</a>
-                        <ul class="dropdown-menu" aria-labelledby="dropdown03">
-                            <li id="nav-to-login" class="dropdown-item" data-route="/login">Login</a></li>
-                            <li id="nav-to-register" class="dropdown-item" data-route="/register">Register</a></li>
-                            <li id="nav-to-dashboard" class="dropdown-item" data-route="/dashboard">Dashboard</a></li>
-                        </ul>
-                    </li>
-                </ul>
-                <form>
-                    <input class="form-control" type="text" placeholder="Search" aria-label="Search">
-                </form>
-            </div>
-        </div>
-    </nav>
-    `;
+    let templateHolder = '';
+    let frag = 'components/navbar/navbar.component';
 
-    function injectTemplate() {
-        NAVBAR_ELEMENT.innerHTML = template;
+    function injectTemplate(callback) {
+
+        if (templateHolder) {
+            NAVBAR_ELEMENT.innerHTML = templateHolder;
+        } else {
+            fetch(`${frag}.html`)
+                .then(resp => resp.text())
+                .then(html => {
+                    templateHolder = html;
+                    NAVBAR_ELEMENT.innerHTML = templateHolder;
+                    callback();
+                })
+                .catch(err => console.error(err));
+        }
+    }
+
+    function injectStylesheet() {
+        let dynamicStyle = document.getElementById('nav-css');
+        if (dynamicStyle) dynamicStyle.remove();
+        dynamicStyle = document.createElement('link');
+        dynamicStyle.id = 'nav-css';
+        dynamicStyle.rel = 'stylesheet';
+        dynamicStyle.href = `${frag}.css`;
+        document.head.appendChild(dynamicStyle);
     }
 
     function navigateToView(e) {
-        console.log(`Navigating to ${e.target.dataset.route}`);
+        router.navigate(e.target.dataset.route);
     }
 
     function logout() {
@@ -48,11 +42,13 @@ function NavbarComponent() {
     }
 
     this.render = function() {
-        injectTemplate();
-        document.getElementById('logout').addEventListener('click', logout);
-        document.getElementById('nav-to-login').addEventListener('click', navigateToView);
-        document.getElementById('nav-to-register').addEventListener('click', navigateToView);
-        document.getElementById('nav-to-dashboard').addEventListener('click', navigateToView);
+        injectStylesheet();
+        injectTemplate(() => {
+            document.getElementById('logout').addEventListener('click', logout);
+            document.getElementById('nav-to-login').addEventListener('click', navigateToView);
+            document.getElementById('nav-to-register').addEventListener('click', navigateToView);
+            document.getElementById('nav-to-dashboard').addEventListener('click', navigateToView);
+        });
     }
 
 }
