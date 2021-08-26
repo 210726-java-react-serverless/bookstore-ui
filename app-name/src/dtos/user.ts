eb8cb212e7ab123cacc0fd2307b59df53af9bd19
Teldu <spsmithsj@gmail.com>
2021-08-26 12:57:39 -0700

    copied over react ui

---
+++ app-name/src/dtos/user.ts
@@ -0,0 +1,18 @@
+export class User{
+
+    id: string;
+    username: string;
+    password: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+    
+    constructor(id: string, un: string, pw: string, fn: string, ln: string, em: string){
+        this.id = id;
+        this.username = un;
+        this.password = pw;
+        this.firstName = fn;
+        this.lastName = ln;
+        this.email = em;
+    }
+}
